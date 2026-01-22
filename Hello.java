fe25c3dcaf21a4f9a8895600f2ed1e2c1f2ff7df
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-01-22 13:14:49 +0500

    first commit

---
+++ Hello.java
@@ -0,0 +1,22 @@
+
+class Hello{
+    public static void main(String a[])
+    {
+        // printName("Zafran Khan");
+        // add(4, 5);
+        name("Gull Khan");
+    }
+
+    static void printName(String name)
+    {
+        System.out.println("Hello, " + name) ;
+    }
+    static int add(int a , int b)
+    {
+        return a + b ;
+    }
+    static String name(String n)
+    {
+        return n;
+    }
+}
