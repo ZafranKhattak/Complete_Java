3ca9636fe288b71fe9b398d6eb1bc0bf60f936b8
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-02-03 14:46:14 +0500

    Loop Added

---
+++ Loop/For_Each_Loop/For_Each_Loop.java
@@ -0,0 +1,13 @@
+// This is used when to access to element directly not by index;
+
+public class For_Each_Loop {
+    
+    public static void main(String[] args) {
+        
+        int [] array = {1,2,3,4,5};
+        for (int num : array)
+        {
+            System.out.print(num+" ");
+        }
+    }
+}
---
+++ Loop/For_Loop/For_Loop.java
@@ -0,0 +1,12 @@
+public class For_Loop {
+    
+    
+    public static void main(String[] args) {
+        
+        int [] array = {1,2,3,4,5};
+        for (int i = 0; i<array.length; i++)
+        {
+            System.out.print(array[i]+" ");
+        }
+    }
+}
