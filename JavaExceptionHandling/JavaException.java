25b67d1436a933327551bb1550075d492cd196ad
ZafranKhattak <zafrankhansoftware.engineer@gmail.com>
2026-04-14 12:10:35 +0500

    modify

--- JavaExceptionHandling/JavaException.java
+++ JavaExceptionHandling/JavaException.java
@@ -1,16 +1,16 @@
-class JavaException
-{
-public static void main(String args[])
-{
-	int a = 5;
-	int b = 0;
-	try{
-	int result = a/b;
-	System.out.println("This will not executed....");
-}
-catch(ArithmeticException e){
-	System.out.println("Number cannot divide by 0");
-}
-System.out.println("Program ends");
-}
+class JavaException {
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 0;
+        try {
+            int result = a / b;
+            System.out.println("This will not executed....");
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide number by 0");
+        } finally {
+            System.out.println("This will always execute even if there is Exception handling in the code ");
+        }
+        System.out.println("Program ends");
+
+    }
 }
